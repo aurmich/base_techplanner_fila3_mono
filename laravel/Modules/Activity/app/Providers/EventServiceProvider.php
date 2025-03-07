60e702609eb6ed4ba28fa4bf30b80cbc1f8d4571
marco76tv <marco.sottana@gmail.com>
2025-03-07 12:50:07 +0100

    up

--- laravel/Modules/Activity/app/Providers/EventServiceProvider.php
+++ laravel/Modules/Activity/app/Providers/EventServiceProvider.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Modules\Activity\Providers;
 
-use Illuminate\Foundation\Support\Providers\EventServiceProvider as BaseEventServiceProvider;
 use Modules\Activity\Listeners\LoginListener;
 use Modules\Activity\Listeners\LogoutListener;
+use Modules\Xot\Providers\XotBaseEventServiceProvider;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as BaseEventServiceProvider;
 
-class EventServiceProvider extends BaseEventServiceProvider
+class EventServiceProvider extends XotBaseEventServiceProvider
 {
     /**
      * The event handler mappings for the application.
