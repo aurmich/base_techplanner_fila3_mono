07d2fff6093db4ec1bbecee522082ce529305aa0
marco76tv <marco.sottana@gmail.com>
2025-09-02 08:28:00 +0200

    .

--- laravel/Themes/Sixteen/resources/views/pages/auth/login.blade.php
+++ laravel/Themes/Sixteen/resources/views/pages/auth/login.blade.php
@@ -9,28 +9,10 @@ name('login');
 
 ?>
 
-<x-layouts.guest>
+<x-layouts.app>
     <x-slot name="title">
-        {{ __('auth.login.title') }}
+        {{ __('Login') }}
     </x-slot>
-
-    <!-- Login Card AGID-Compliant (Componente Corretto) -->
-    <x-pub_theme::blocks.forms.login-card 
-        title="{{ __('auth.login.title') }}"
-        subtitle="{{ __('auth.login.description', ['service' => config('app.name')]) }}"
-        livewire-component="\Modules\User\Http\Livewire\Auth\Login"
-    />
-
-    <!-- Registration Link (if enabled) -->
-    @if (Route::has('register'))
-        <div class="text-center mt-6">
-            <p class="text-sm text-gray-600">
-                {{ __('auth.login.no_account') }}
-                <a href="{{ route('register') }}" 
-                   class="text-blue-600 hover:text-blue-800 underline font-medium">
-                    {{ __('auth.login.create_account') }}
-                </a>
-            </p>
-        </div>
-    @endif
-</x-layouts.guest>
+    @livewire(Modules\User\Filament\Widgets\Auth\LoginWidget::class)
+   
+</x-layouts.app>
